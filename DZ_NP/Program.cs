c3367a6fb6d9f1af33e292c1a297ccf18383902b
Микола Матіос <mikolka09@gmail.com>
2021-03-09 23:39:56 +0200

    Добавьте файлы проекта.

---
+++ DZ_NP/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace DZ_NP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress iP = IPAddress.Parse("127.0.0.1");
+            IPEndPoint iPEnd = new IPEndPoint(iP, 8008);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Bind(iPEnd);
+                socket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+                while (true)
+                {
+                    Socket socket1 = socket.Accept();
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = socket1.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socket1.Available > 0);
+                    string message = "";
+                    if (builder.ToString() == "1")
+                        message = DateTime.Now.ToShortDateString();
+                    if (builder.ToString() == "2")
+                        message = DateTime.Now.ToShortTimeString();
+                    data = Encoding.Unicode.GetBytes(message);
+                    socket1.Send(data);
+                    socket1.Shutdown(SocketShutdown.Both);
+                    socket1.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DZ_NP_Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace DZ_NP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool res = true;
+                while (res)
+                {
+                    IPAddress iP = IPAddress.Parse("127.0.0.1");
+                    IPEndPoint iPEnd = new IPEndPoint(iP, 8008);
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(iPEnd);
+                    Console.WriteLine("Какие данные получить от сервера:\n" +
+                                      "1. Дата\n2. Время\n3. Выход\n");
+                    Console.Write("Ваш выбор: ");
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+
+                    data = new byte[256];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socket.Available > 0);
+                    Console.WriteLine();
+                    Console.WriteLine("Ответ сервера: " + builder.ToString());
+                    Console.WriteLine();
+                    Console.Write("Желаете сделать еще запрос (Да-1, Нет-2): ");
+                    string var = Console.ReadLine();
+                    if (message == "3" || var == "2")
+                    {
+                        socket.Shutdown(SocketShutdown.Both);
+                        socket.Close();
+                        res = false;
+                    }
+                    if (var == "1")
+                    {
+                        Console.Clear();
+                        socket.Shutdown(SocketShutdown.Both);
+                        socket.Close();
+                        res = true;
+                    }
+                }
+                Console.WriteLine("\nСвязь с сервером прервана! Для выхода нажмите любую кнопку!");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
